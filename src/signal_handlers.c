a8039eba742728982db961bc952201889e063915
aeis <aiis7905@hanmail.net>
2017-11-14 14:11:48 +0900

    change signa

--- src/signal_handlers.c
+++ src/signal_handlers.c
@@ -1,11 +1,18 @@
+#include <signal.h>
 #include "signal_handlers.h"
 
 void catch_sigint(int signalNo)
 {
   // TODO: File this!
+
+        signal(signalNo,SIG_IGN);
+
 }
 
 void catch_sigtstp(int signalNo)
 {
   // TODO: File this!
+
+        signal(signalNo,SIG_IGN);
 }
+
